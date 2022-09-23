f5b4f1dca4bcbbb624036df32e134d49ebbda07a
nikhil-RGB <bleach.ichigo.anime@gmail.com>
2022-09-23 23:22:22 +0530

    Add default profile icon, remove and unregister assets

--- to_do_app/lib/all-screens/home.dart
+++ to_do_app/lib/all-screens/home.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/all-const/colors.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: new Text('To-Do App')),
+      appBar: AppBar(
+          elevation: 0,
+          backgroundColor: tdBGColor,
+          title: new Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Icon(
+                Icons.menu,
+                color: tdBlack,
+                size: 30,
+              ),
+              Container(
+                  height: 40,
+                  width: 40,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(30),
+                    child: Icon(
+                      Icons.account_circle_rounded,
+                      color: Colors.blue.shade400,
+                    ),
+                  ))
+            ],
+          )),
       body: Container(
         child: Text('This is my homepage'),
       ),
--- to_do_app/lib/main.dart
+++ to_do_app/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'To-Do App',
       home: new Home(),
     );
