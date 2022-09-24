88d740d39c3d9f6a74b244519eb906db7002b257
nikhil-RGB <bleach.ichigo.anime@gmail.com>
2022-09-24 07:21:46 +0530

    Add and style searchbar

--- to_do_app/lib/all-screens/home.dart
+++ to_do_app/lib/all-screens/home.dart
@@ -5,32 +5,63 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          elevation: 0,
-          backgroundColor: tdBGColor,
-          title: new Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Icon(
-                Icons.menu,
-                color: tdBlack,
-                size: 30,
-              ),
-              Container(
-                  height: 40,
-                  width: 40,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(30),
-                    child: Icon(
-                      Icons.account_circle_rounded,
-                      color: Colors.blue.shade400,
-                    ),
-                  ))
-            ],
-          )),
+      appBar: buildBar(),
       body: Container(
-        child: Text('This is my homepage'),
+          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          child: Column(
+            children: [createSearcher()],
+          )),
+    );
+  }
+
+  //Creates the search box text field for the home screen.
+  Container createSearcher() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+        color: tdBGColor,
+        borderRadius: BorderRadius.circular(25),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(0),
+          prefixIcon: Icon(Icons.search, color: tdBlack, size: 20),
+          prefixIconConstraints: BoxConstraints(
+            maxHeight: 20,
+            minWidth: 25,
+          ),
+          border: InputBorder.none,
+          hintText: 'Search To-Do here',
+          hintStyle: TextStyle(color: tdGrey),
+        ),
       ),
     );
+  }
+
+  //This function is used to build the app bar to be used in the application's homescreen.
+  AppBar buildBar() {
+    return AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: new Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(
+              Icons.menu,
+              color: tdBlack,
+              size: 30,
+            ),
+            Container(
+                height: 40,
+                width: 40,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(30),
+                  child: Icon(
+                    Icons.account_circle_rounded,
+                    color: Colors.blue.shade400,
+                  ),
+                ))
+          ],
+        ));
   }
 }
--- to_do_app/lib/main.dart
+++ to_do_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_app/all-screens/home.dart';
 import 'package:to_do_app/all-const/colors.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +13,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of my mobile application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'To-Do App',
